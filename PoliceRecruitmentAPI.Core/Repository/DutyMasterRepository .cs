5081522ca895721299ee6b7bdc5b19b6b02e299c
runali40 <runali.devit@gmail.com>
2025-02-19 08:34:49 -0700

    change 423 error

--- PoliceRecruitmentAPI.Core/Repository/DutyMasterRepository .cs
+++ PoliceRecruitmentAPI.Core/Repository/DutyMasterRepository .cs
@@ -106,7 +106,7 @@ namespace PoliceRecruitmentAPI.Core.Repository
                     {
                         return new ObjectResult(result)
                         {
-                            StatusCode = 408
+                            StatusCode = 423
                         };
                     }
                     else
--- PoliceRecruitmentAPI.Core/Repository/UserMasterRepository.cs
+++ PoliceRecruitmentAPI.Core/Repository/UserMasterRepository.cs
@@ -62,7 +62,7 @@ namespace PoliceRecruitmentAPI.Core.Repository
                     {
                         return new ObjectResult(result)
                         {
-                            StatusCode = 408
+                            StatusCode = 423
                         };
                     }
                     else
