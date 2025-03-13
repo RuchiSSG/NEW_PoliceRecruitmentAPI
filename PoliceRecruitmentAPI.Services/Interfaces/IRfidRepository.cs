58db7811a2722c35e7e35daa8811c0d78c3f7380
Ashish Joshi <Ashish@gmail.com>
2025-03-13 10:56:34 +0530

    rfidtag read

---
+++ PoliceRecruitmentAPI.Services/Interfaces/IRfidRepository.cs
@@ -0,0 +1,18 @@
+﻿using PoliceRecruitmentAPI.Services.ApiServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoliceRecruitmentAPI.Core;
+using PoliceRecruitmentAPI.Core.ModelDtos;
+
+namespace PoliceRecruitmentAPI.Services.Interfaces
+{
+    public interface IRfidRepository
+    {
+        Task StartListenerAsync();
+        Task StopListener();
+        Task<RfidResult> GetLatestTagAsync();
+    }
+}
---
+++ PoliceRecruitmentAPI/Controllers/RFIDController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PoliceRecruitmentAPI.Services.Interfaces;
+
+namespace PoliceRecruitmentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RFIDController : ControllerBase
+    {
+        private readonly IRfidRepository _rfidRepository;
+        private static string _controllerCachedTag = null;
+
+        public RFIDController(IRfidRepository rfidRepository)
+        {
+            _rfidRepository = rfidRepository;
+        }
+
+        [HttpPost("start")]
+        public async Task<IActionResult> StartListener()
+        {
+            await _rfidRepository.StartListenerAsync();
+            //return Ok("RFID listener started.");
+
+            return NoContent();
+        }
+
+
+        [HttpPost("stop")]
+        public IActionResult StopListener()
+        {
+            _rfidRepository.StopListener();
+            // return Ok("RFID listener stopped.");
+            return NoContent();
+        }
+
+        [HttpGet("ReadRFIDtag")]
+        public async Task<IActionResult> GetLatestTag()
+        {
+
+            var result = await _rfidRepository.GetLatestTagAsync();
+            return Ok(result);
+        }
+    }
+}
+
+
