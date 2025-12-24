141e5f533668625786ca0f20101023cfa6ef8ff3
RuchiSSg <ruchi0533@gmail.com>
2025-12-24 09:14:05 +0530

    rfid

--- PoliceRecruitmentAPI.Core/ModelDtos/RFIDRunningLogItem.cs
+++ PoliceRecruitmentAPI.Core/ModelDtos/RFIDRunningLogItem.cs
@@ -8,7 +8,16 @@ namespace PoliceRecruitmentAPI.Core.ModelDtos
 {
     public class RFIDRunningLogItem
     {
-        public string RFIDdtagata { get; set; }
+        public string? RFIDdtagata { get; set; }
+        public string? Lap1 { get; set; }
+        public string? Lap2 { get; set; }
+        public string? Lap3 { get; set; }
+        public string? Lap4 { get; set; }
+        public string? Lap5 { get; set; }
+        public int TotalLaps { get; set; }
+        public List<string>? AllLaps { get; set; }
+        public string? EventName { get; set; }
+       // public string RFIDdtagata { get; set; }
         public string Timestamp { get; set; }
     }
 }
