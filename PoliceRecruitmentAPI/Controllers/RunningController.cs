f73c0ec84276cebf68aaaa570316fd9c1f420e16
RuchiSSG <ruchi0533@gmail.com>
2026-01-20 10:28:32 +0530

    InsertAppeal

--- PoliceRecruitmentAPI/Controllers/RunningController.cs
+++ PoliceRecruitmentAPI/Controllers/RunningController.cs
@@ -288,7 +288,51 @@ namespace PoliceRecruitmentAPI.Controllers
             }
 
         }
-		[HttpPost("Update")]
+
+        [HttpPost("InsertAppeal")]
+        public async Task<IActionResult> InsertAppeal([FromBody] RunningDto user)
+        {
+            try
+            {
+                if (user.BaseModel == null)
+                {
+                    user.BaseModel = new BaseModel();
+                }
+                user.BaseModel.OperationType = "InsertAppeal";
+                var result = await _candidateService.Running(user);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                // Using LogErrorResponse model for cleaner code
+                var errorResponse = new LogErrorResponse
+                {
+                    ErrorId = Guid.NewGuid().ToString("N"),
+                    Timestamp = DateTime.Now,
+                    Message = ex.Message,
+                    StackTrace = ex.StackTrace,
+                    OperationType = user?.BaseModel?.OperationType ?? "Unknown"
+                };
+
+                // Log error details
+                _logger.LogError(ex, "{SeparatorLine}\n"+"Error ID: {ErrorId}\t" +"DateTime: {FormattedTimestamp}\n" +"Error Message: {Message}\n" +"Stack Trace: {StackTrace}\n"+"{SeparatorLine}",
+                     LogErrorResponse.SEPARATOR_LINE,
+                     errorResponse.ErrorId,
+                     errorResponse.FormattedTimestamp,
+                     errorResponse.Message,
+                     errorResponse.StackTrace,
+                     LogErrorResponse.SEPARATOR_LINE
+                 );
+
+                return new JsonResult(errorResponse)
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
+            }
+
+        }
+
+        [HttpPost("Update")]
 		public async Task<IActionResult> Update([FromBody] RunningDto user)
 		{
 			try
